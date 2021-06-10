faf4d3da43e7a00ae0931891df608a903f554265
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2021-06-10 17:21:04 +0500

    boilerplate code for connecting metamask

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import Wallet from './components/Wallet'
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 8000;
+  return library;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary} >
+      <Wallet />
+    </Web3ReactProvider>
   );
 }
 
---
+++ src/components/Wallet.js
@@ -0,0 +1,202 @@
+import { useEffect, useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { metaMask } from '../connectors';
+import { formatEther } from '@ethersproject/units';
+
+const Wallet = () => {
+  const {
+    connector,
+    library,
+    account,
+    chainId,
+    activate,
+    deactivate,
+    active,
+    error,
+  } = useWeb3React();
+
+  const [blockNumber, setBlockNumber] = useState(undefined);
+  const [ethBalance, setEthBalance] = useState();
+  const [activatingConnector, setActivatingConnector] = useState();
+
+  let connected = connector === metaMask;
+  const disabled = !!activatingConnector || connected || !!error;
+
+  useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined);
+    }
+  }, [activatingConnector, connector]);
+
+  useEffect(() => {
+    console.log(library);
+    if (library) {
+      library.getBlockNumber().then((bn) => {
+        setBlockNumber(bn);
+      });
+      library.on('block', setBlockNumber);
+      return () => {
+        library.removeListener('block', setBlockNumber);
+        setBlockNumber(undefined);
+      };
+    }
+  }, [library]);
+
+  useEffect(() => {
+    console.log('running');
+    if (library && account) {
+      let stale = false;
+
+      library
+        .getBalance(account)
+        .then((balance) => {
+          if (!stale) {
+            setEthBalance(balance);
+          }
+        })
+        .catch(() => {
+          if (!stale) {
+            setEthBalance(null);
+          }
+        });
+
+      return () => {
+        stale = true;
+        setEthBalance(undefined);
+      };
+    }
+  }, [library, account, chainId]);
+
+  const onClickActivate = () => {
+    activate(metaMask);
+    setActivatingConnector(metaMask);
+  };
+
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1 style={{ margin: '0', textAlign: 'right' }}>
+        {active ? '🟢' : error ? '🔴' : '🟠'}
+      </h1>
+      <h3
+        style={{
+          display: 'grid',
+          gridGap: '1rem',
+          gridTemplateColumns: '1fr min-content 1fr',
+          maxWidth: '20rem',
+          lineHeight: '2rem',
+          margin: 'auto',
+        }}
+      >
+        <span>Chain Id</span>
+        <span role="img" aria-label="chain">
+          ⛓
+        </span>
+        <span>{chainId === undefined ? '...' : chainId}</span>
+
+        <span>Block Number</span>
+        <span role="img" aria-label="numbers">
+          🔢
+        </span>
+
+        <span>
+          {blockNumber === undefined
+            ? '...'
+            : blockNumber === null
+            ? 'Error'
+            : blockNumber.toLocaleString()}
+        </span>
+
+        <span>Account</span>
+        <span role="img" aria-label="robot">
+          🤖
+        </span>
+        <span>
+          {account === undefined
+            ? '...'
+            : account === null
+            ? 'None'
+            : `${account.substring(0, 6)}...${account.substring(
+                account.length - 4
+              )}`}
+        </span>
+
+        <span>Balance</span>
+        <span role="img" aria-label="gold">
+          💰
+        </span>
+        <span>
+          {ethBalance === undefined
+            ? '...'
+            : ethBalance === null
+            ? 'Error'
+            : `Ξ${parseFloat(formatEther(ethBalance)).toPrecision(4)}`}
+        </span>
+      </h3>
+
+      <button
+        style={{
+          height: '3rem',
+          borderRadius: '1rem',
+          marginTop: '5%',
+          left: '40%',
+          borderColor:
+            activatingConnector === metaMask
+              ? 'orange'
+              : metaMask
+              ? 'green'
+              : 'unset',
+          cursor: disabled ? 'unset' : 'pointer',
+          position: 'relative',
+        }}
+        disabled={disabled}
+        key={metaMask}
+        onClick={() => {
+          onClickActivate();
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            color: 'black',
+            margin: '0 0 0 1rem',
+          }}
+        >
+        </div>
+        {'Connect MetaMask'}
+      </button>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {(active || error) && (
+          <button
+            style={{
+              height: '3rem',
+              marginLeft: '10%',
+              marginTop: '-4%',
+              borderRadius: '1rem',
+              borderColor: 'red',
+              cursor: 'pointer',
+            }}
+            onClick={() => {
+              deactivate();
+            }}
+          >
+            Deactivate
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Wallet;
